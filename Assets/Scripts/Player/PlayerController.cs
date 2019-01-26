256852a26198f86955278854c4a1c7730d13a9a3
Pedro Bandeira <pedro.bandeira@tap4mobile.com.br>
2019-01-26 11:52:11 -0400

    Adding the player controllers - Currently ongoing

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 6.0f, jumpSpeed = 8.0f, dashLength = 2.0f, dashRefillRate = 5.0f;
+    [SerializeField]
+    int numberOfDashesAvailable = 1;
+
+    enum PlayerState
+    {
+        idle, walking, attacking
+    }
+
+    PlayerState currentState;
+    private Vector3 lastPosition = new Vector3(0, 0, 0);
+    private float gravity = 20.0f;
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        //gameObject.transform.position = new Vector3(0, 5, 0);
+    }
+
+    void Update()
+    {
+        MovePlayer();
+
+        if (lastPosition != gameObject.transform.position)
+        {
+            GetComponent<Animator>().SetBool("Idle", false);
+            GetComponent<Animator>().SetTrigger("Walking");
+        }
+        else
+        {
+
+            GetComponent<Animator>().SetBool("Idle", true);
+
+        }
+
+        lastPosition = gameObject.transform.position;
+        // Apply movement based on gravity
+        moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
+
+        // Move the controller
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    private void MovePlayer()
+    {
+        if (controller.isGrounded)
+        {
+            currentState = PlayerState.walking;
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection = moveDirection * speed;
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/Player/PlayerStateMachine.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateMachine : MonoBehaviour
+{
+    Animator playerAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    void IdleState()
+    {
+        playerAnimator.SetBool("Idle", true);
+    }
+
+    void WalkingState()
+    {
+        playerAnimator.SetTrigger("Walking");
+    }
+
+    void TookHitState()
+    {
+        playerAnimator.SetTrigger("TookHit");
+    }
+
+    void AttackingState()
+    {
+        playerAnimator.SetTrigger("Attacking");
+    }
+
+    void DashingState()
+    {
+        playerAnimator.SetTrigger("Dashing");
+    }
+
+    void DeadState()
+    {
+        playerAnimator.SetBool("Dead", true);
+    }
+}
