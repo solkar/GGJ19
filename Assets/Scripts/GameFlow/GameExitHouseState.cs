03106f128930df079b05e6ddda670adbb68d0e01
Jon Ander Hernandez <jonan.h@gmail.com>
2019-01-26 17:28:11 +0200

    Add FSM skeleton to implement the game flow

---
+++ Assets/Scripts/GameFlow/GameExitHouseState.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using System.Collections;
+
+public class GameExitHouseState : IFSMState
+{
+    [Serializable]
+    public struct Settings
+    {
+        [SerializeField]
+        public string houseScene;
+    }
+
+    private Settings settings;
+
+    public GameExitHouseState(
+        Settings settings
+    )
+    {
+        this.settings = settings;
+    }
+
+    public IEnumerator Enter()
+    {
+        yield break;
+    }
+
+    public void Exit()
+    {
+    }
+}
---
+++ Assets/Scripts/GameFlow/GameStateFSM.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class GameStateFSM : MonoBehaviour
+{
+    public enum State
+    {
+        GameStarting,
+        EnterHouse,
+        ExitHouse,
+        Ending,
+        End,
+    }
+
+    #region serializable fields
+
+    [SerializeField]
+    private GameEnterHouseState.Settings enterHouse;
+
+    [SerializeField]
+    private GameExitHouseState.Settings exitHouse;
+
+    #endregion
+
+    [SerializeField]
+    private State state;
+    private float lastStateChange;
+    private State stateRequest;
+    private Coroutine lastCoroutine;
+
+    protected void Awake()
+    {
+        //EventBus.OnEnterHouse.evt += () => { stateRequest = State.EnterHouse; };
+        //EventBus.OnExitHouse.evt += () => { stateRequest = State.ExitHouse; };
+    }
+
+    IEnumerator Start()
+    {
+        // Initialize states of the state machine
+        var enterHouseState = new GameEnterHouseState(enterHouse);
+        var exitHouseState = new GameExitHouseState(exitHouse);
+
+        yield return null;
+
+        while (true)
+        {
+            var newState = state;
+            try
+            {
+                // Evaluate conditions to change the behaviour
+                {
+                }
+
+                if (newState != state)
+                {
+                    // Exit the previous state
+                    if (lastCoroutine != null)
+                    {
+                        Debug.Log("Stopping previous state " + state);
+
+                        StopCoroutine(lastCoroutine);
+
+                        switch (state)
+                        {
+                            case State.EnterHouse:
+                                enterHouseState.Exit();
+                                break;
+
+                            case State.ExitHouse:
+                                enterHouseState.Exit();
+                                break;
+                        }
+                    }
+
+                    Debug.Log("Starting new state state " + newState);
+
+                    // Enter the new state
+                    switch (newState)
+                    {
+                        case State.EnterHouse:
+                            {
+                                lastCoroutine = StartCoroutine(enterHouseState.Enter());
+                            }
+                            break;
+
+                        case State.ExitHouse:
+                            {
+                                lastCoroutine = StartCoroutine(exitHouseState.Enter());
+                            }
+                            break;
+
+                    }
+                    state = newState;
+                    lastStateChange = Time.time;
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Fuuuuuck!");
+                Debug.LogException(e);
+                newState = State.End;
+            }
+
+            yield return null;
+        }
+    }
+}
