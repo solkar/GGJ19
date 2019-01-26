c4c1efdb4b9bb4c0c999ae968df7f6eaf3e9e4b0
Karlos Zafra <karlos.zafra@nextgames.com>
2019-01-26 21:14:09 +0200

    Play clips on player events

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,18 +1,40 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class SoundManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Serializable]
+    public struct SoundBank
     {
+        [SerializeField]
+        public AudioClip playerAttack;
         
+        [SerializeField]
+        public AudioClip playerDamage;
+    }
+    
+    [SerializeField]
+    private SoundBank soundBank;
+    
+    private AudioSource _audioSource;
+    
+    private void Awake()
+    {
+        var audioSourceObject = transform.Find("MainCamera");
+        Assert.IsNotNull(audioSourceObject);
+        _audioSource = audioSourceObject.GetComponent<AudioSource>();
+            
+        EventBus.OnPlayerAttack.evt += () => { PlaySingle(soundBank.playerAttack); };
+        EventBus.OnPlayerDamage.evt += () => { PlaySingle(soundBank.playerDamage); };
+    }
+    
+    public void PlaySingle(AudioClip clip)
+    {
+        _audioSource.clip = clip;
+        _audioSource.Play ();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
