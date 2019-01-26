d6b22b21f9d99b0104cf768bdf09c3c6c4f148ed
Karlos Zafra <karlos.zafra@nextgames.com>
2019-01-27 00:48:16 +0200

    Recover full Hp when entering the home

---
+++ Assets/Scripts/UI/HpDisplayController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class HpDisplayController : MonoBehaviour
+{
+    [SerializeField] private int _maxHealth = 5;
+    
+    [SerializeField] 
+    private UnitDisplayController _displayController;
+    
+    void Start()
+    {
+        Assert.IsNotNull(_displayController);
+
+        EventBus.OnPlayerHeal.evt += (int amount) => { _displayController.counter++;};
+    }
+
+    private void Update()
+    {
+        // This should be done in a Player component, not in the UI
+        if (_displayController.counter == _maxHealth)
+        {
+              EventBus.OnPlayerHealthMax.Invoke();
+         }
+    }
+}
