ddcc4d6c7557b4b8e161e2276d19599418bf8530
Jon Ander Hernandez <jonan.h@gmail.com>
2019-01-26 19:38:23 +0200

    Add EnemyWandering and EnemyNavMeshSearch enemy AI states

---
+++ Assets/Scripts/Enemies/AI/Tests/EnemyWanderingTest/EnemyWanderingTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyWanderingTest : MonoBehaviour
+{
+    #region Serialized
+
+    [SerializeField]
+    public NavMeshAgent enemyAgent;
+
+    [SerializeField]
+    public Enemies.EnemyWandering settings;
+
+    #endregion
+
+    IEnumerator Start()
+    {
+        var wanderingState = new Enemies.EnemyWandering.State(
+            settings.settings,
+            enemyAgent
+        );
+        
+        StartCoroutine(wanderingState.Enter());
+
+        yield break;
+    }
+}
