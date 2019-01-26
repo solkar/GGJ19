760557719f2c0a31e6894679c66e55ec26c48422
Karlos Zafra <karlos.zafra@nextgames.com>
2019-01-26 12:05:57 +0200

    Add component to countrol Hud counters

---
+++ Assets/Scripts/UI/UnitDisplayController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class UnitDisplayController : MonoBehaviour
+{
+    public int counter;
+    
+    private List<Transform> _childrenList = new List<Transform>();
+    private void Awake()
+    {
+        transform.GetComponentsInChildren<Transform>(true,_childrenList);
+        
+        _childrenList.Remove(transform);
+        
+        Assert.IsTrue(_childrenList.Count > 0);
+    }
+
+    void Update()
+    {
+        counter = Mathf.Min(counter, _childrenList.Count);
+        
+        for (int i = 0; i < _childrenList.Count; i++)
+        {
+            if (i < counter)
+            {
+                _childrenList[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                _childrenList[i].gameObject.SetActive(false);
+            }
+        }
+
+    }
+}
