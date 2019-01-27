fd3cb4cf8da9b3baeb08d8262904fe2748bb2140
Karlos Zafra <karlos.zafra@nextgames.com>
2019-01-27 11:47:57 +0200

    Add Inventory singleton to services

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : Singleton<Inventory>
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
